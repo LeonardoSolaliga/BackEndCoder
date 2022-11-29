ad993647066af14a9a373cdbe224ab774ca6a87d
unknown <leo_nosecuanto@hotmail.com>
2022-11-29 11:20:27 -0300

    parte handlebars

---
+++ practica_5/hbs.js
@@ -0,0 +1,35 @@
+//let Contenedor = require('./contenedor.js');
+//let conten=new Contenedor('./productos.txt');
+const Contenedor=require("./Contenedor.js");
+const productos=new Contenedor("./productos.txt");
+let express=require("express");
+let PORT=8080;
+let app=express();
+let hbs = require("express-handlebars");
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.engine('handlebars', hbs.engine());
+
+app.set("views", "./views/hbs");
+app.set("view engine", "handlebars");
+
+
+app.get("/", (req, res)=>{
+    res.render("formulario",{});
+});
+
+app.get('/productos',async(req,res,next) => {
+    let allProducts = await productos.getAll()
+    let productLength=allProducts?.length>0 ? true:false;
+    res.render("productos",{allProducts,productLength})
+})
+
+app.post("/productos",async(req,res,next)=>{
+    let obj=req.body;
+    await productos.save(obj);
+    res.redirect("/productos")
+})
+
+app.listen(PORT);
