96b60024b233cd9f893683ecd554ccbcca00ab38
unknown <leo_nosecuanto@hotmail.com>
2023-02-01 16:20:26 -0300

    eliminar carritos y productos del carrito

--- segunda_entrega/DAOs/MongoDB/mongocarts.js
+++ segunda_entrega/DAOs/MongoDB/mongocarts.js
@@ -19,15 +19,17 @@ class ContenedorCartsMongo extends ContainerMongo {
         let now = today.toLocaleString();
         let timestamp=now;
         let carritos=await this.getAll();
+        console.log(carritos);
         let id;
-        id=carritos.length===0 ? 1:carritos[carritos.length-1].id+1;
+        id=carritos.length===0 ? 1:carritos[carritos.length-1].cartId+1;
         let cart={cartId:id,timestamp:timestamp,productos:productos};
+        console.log(cart.cartId);
         let newCart = await this.collection.create(cart);
         return newCart;
     };
     async getById(id){
-        let product=await this.collection.find({Productid:id})
-        return product;
+        let cart=await this.collection.find({cartId:id})
+        return cart;
 
     }
 
@@ -68,6 +70,9 @@ class ContenedorCartsMongo extends ContainerMongo {
         let prueba=arr.filter(elem=>elem.Productid!=producto.Productid)
         await this.collection.updateOne({cartId:carid},{$set:{productos:prueba}})
         console.log(prueba);
+    }
+    async deleteById(id){
+        await this.collection.deleteOne({cartId:id});
     }
 
 };
